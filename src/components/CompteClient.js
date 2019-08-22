b003ed2999482b93ca7efe1ef28dba26fa2d0491
stageparfait4 <stageparfait4@gmail.com>
2019-08-22 18:37:05 +0200

    Ajout de la page de creation de compte avec succes

--- src/components/CompteClient.js
+++ src/components/CompteClient.js
@@ -1,5 +1,9 @@
 import React, { Component } from 'react';
-import '../css/CompteClient.css';
+import '../css/creerCompte.css';
+import Connexion from './Connexion';
+import '../css/Connexion.css';
+import ReactDOM from 'react-dom';
+import { Link } from 'react-dom'
 
 class Affiche extends Component {
     state = {
@@ -8,8 +12,17 @@ class Affiche extends Component {
         Numero: null,
         Password: null,
         Ville: null,
-        Quartier: null
+        Quartier: null,
+        affiche: false
     }
+
+    handleClick = (e) =>{
+        this.setState({
+            affiche:true
+        })
+        // e.preventDefault();
+    }
+
     submit = (e) => {
         e.preventDefault();
         console.log(this.state);
@@ -26,20 +39,18 @@ class Affiche extends Component {
     }
 
     render() {
-        return (
+        return this.state.affiche===false?(
             <div className = "initial"> 
                 <div className="form-wrapper" >
                     <h1>Entrez vos coordonnees</h1>
                     <form onSubmit={this.handleSubmit} novalidate>
                         <div className="left">
-                            <label htmlFor="Nom"></label>
-                            <input type="text" placeholder="Nom" name="Nom" onChange={this.Change} novalidate/><br /><br />
+                            <input className = "inputCreer" type="text" placeholder="Nom" name="Nom" onChange={this.Change} novalidate/><br /><br />
                             
-                            <label htmlFor="Numero"></label>
-                            <input type="text" placeholder="Numero" name="Numero" onChange={this.Change} novalidate/><br />
+                            <input className = "inputCreer" type="text" placeholder="Numero" name="Numero" onChange={this.Change} novalidate/><br />
                             
-                            <label htmlFor="Ville" className="label">Ville</label><br />
-                            <select name="Ville" value="armand">
+                            <label htmlFor="Ville" className="labelCreer">Ville</label><br />
+                            <select className="selectCreer" name="Ville" value="armand">
                                 <option value="Douala">Douala</option>
                                 <option value="Yaounde">Yaounde</option>
                                 <option value="Bafoussam">Bafoussam</option>              
@@ -47,27 +58,26 @@ class Affiche extends Component {
                         </div>
 
                         <div className="right">
-                            <label htmlFor="Email"></label>
-                            <input type="email" placeholder="Email" name="Email" onChange={this.Change} novalidate/><br /><br />
+                            <input className = "inputCreer" type="email" placeholder="Email" name="Email" onChange={this.Change} novalidate/><br /><br />
 
-                            <label htmlFor="Password"></label>
-                            <input type="password" placeholder="Mot de passe" name="Password" onChange={this.Change} novalidate/><br/>
+                            <input className = "inputCreer" type="password" placeholder="Mot de passe" name="Password" onChange={this.Change} novalidate/><br/>
 
-                            <label htmlFor="Quatier" className="label">Quartier</label><br />
-                            <select name="Quatier" value="Quartier">
+                            <label htmlFor="Quatier" className="labelCreer">Quartier</label><br />
+                            <select className="selectCreer" name="Quatier" value="Quartier">
                                 <option value="elfe">elfe</option>
                                 <option value="minboman">minboman</option>
                                 <option value="camkop">camkop</option>
                             </select><br/><br/><br/>
                         </div>
                     </form>
-
-                    <div className="Creer">
-                            <button type="Submit"> Creer </button>
+                    <div >
+                        <button className="buttonCreer" type="Submit"> Creer </button><br/>
+                        <a id="creer" href = "/" onClick={this.handleClick}>Se connecter</a>
                     </div>
-
                 </div>
             </div>
+        ):(
+                ReactDOM.render(<Connexion />, document.getElementById('root'))
         )
     }
 }
--- src/components/CreerCompte.js
+++ src/components/CreerCompte.js
@@ -1,53 +1,88 @@
 import React, {Component} from "react";
 import Affiche from './CompteClient';
-import "../css/CompteClient.css";
-import LogoCenter from "./LogoCenter"
+import logo from '../assets/logo.png';
+import '../css/creerCompte.css'
+import CompteResto from './AfficheForm'
 
 
-class CompteClient extends Component { 
+class CreerCompte extends Component { 
     constructor(props){
         super(props);
-        this.state = { Nom: false};
+        this.state = {
+            afficheQui: "rien"
+        };
     }
+
     init = (e) => {
-            console.log(e.target.id)
+           // console.log(e.target.id)
             return e.target.id === "client"? (<Affiche />): null;
     }
+
     displayScreen = (e) => {
-        return e.target.id === "client"? (this.setState({
-            Nom: true
-        })):(this.setState({
-            Nom: false
+        console.log(e.target.id)
+        e.target.id == "client"?
+        (this.setState({
+            afficheQui: "client"
+        }))
+        :(this.setState({
+            afficheQui: "resto"
         }));
+        console.log(this.state.afficheQui)
     }
+
     submit =(e) => {
         e.preventDefault();
         console.log(this.state);
     }
     render(){
-        return this.state.Nom === true? (
-            <div className="red">
-                <LogoCenter />
-                <form onSubmit={this.submit} className="formInitial">
-                    <label htmlFor="resto">Restaurant</label>
-                    <input type="radio" name='choice'id="res" onClick={this.displayScreen}/>
+        if(this.state.afficheQui === "rien"){
+            return(
+                <div className="red">
+                    <img src={logo} alt="logo" height="100px" width="96px" /><br/>
+                    <div className="initial"><br/>
+                        <form onSubmit={this.submit} className="formInitial">
+                            <label htmlFor="accountChoice">Restaurant</label>
+                            <input type="radio" name='accountChoice'id="res" onClick={this.displayScreen}/>
 
-                    <label htmlFor="client">client</label>
-                    <input type="radio" name='choise'id="client" checked="true" onClick={this.displayScreen}/>
-                </form><br/>
-                <Affiche sow={this.displayScreen}/>
-            </div>
-        ):(
-            <div className="initial">
-            <LogoCenter/><br /><br /><br /><br /><br />
-                <form onSubmit={this.submit} className ="formInitial">
-                    <label htmlFor="restaurant">Restaurant</label>
-                    <input type="radio" name='choice'id="res" onClick={this.displayScreen}/>
-                    <label htmlFor="client">client</label>
-                    <input type="radio" name='choise'id="client" onClick={this.displayScreen}/>
-                </form>
-            </div>
-        );
+                            <label htmlFor="client">client</label>
+                            <input type="radio" name='accountChoice'id="client" onClick={this.displayScreen}/>
+                        </form>
+                    </div>
+                </div>
+            )
+        }else if(this.state.afficheQui === "client"){
+            return(
+                <div className="red">
+                    <img src={logo} alt="logo" height="100px" width="96px" /><br/>
+                    <div className="initial"><br/>
+                        <form onSubmit={this.submit} className="formInitial">
+                            <label htmlFor="accountChoice">Restaurant</label>
+                            <input type="radio" name='accountChoice'id="res" onClick={this.displayScreen}/>
+    
+                            <label htmlFor="client">client</label>
+                            <input type="radio" name='accountChoice'id="client" checked="true" onClick={this.displayScreen}/>
+                        </form><br/>
+                        <Affiche sow={this.displayScreen}/>
+                    </div>
+                </div>
+            )
+        }else{
+            return(
+                <div className="red">
+                    <img src={logo} alt="logo" height="100px" width="96px" /><br/>
+                    <div className="initial"><br />
+                        <form onSubmit={this.submit} className ="formInitial">
+                            <label htmlFor="accountChoice">Restaurant</label>
+                            <input type="radio" name='accountChoice' id="res" onClick={this.displayScreen}/>
+                            <label htmlFor="client">client</label>
+                            <input type="radio" name='accountChoice' id="client" onClick={this.displayScreen}/>
+                        </form><br />
+                        <CompteResto />
+                    </div>
+                </div>
+            )
         }
     }
-    export default CompteClient;
+}
+
+export default CreerCompte;
